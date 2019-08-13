0d73b41d9deb09a5930c0102b5ad885bfee567af
Mansilla <chusanic@gmail.com>
2019-08-13 00:51:04 -0600

    first commit

---
+++ Game/Doublylinkedlist.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, data):
+        self.item = data
+        self.nref = None
+        self.pref = None
+
+    def get_data(self):
+        return self.item
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.start_prev = None
+        self.start_node = None
+
+    def insert_in_emptylist(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+        else:
+            print("list is not empty")
+
+    def insert_at_start(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            print("node inserted")
+            return
+        new_node = Node(data)
+        new_node.nref = self.start_node
+        self.start_node.pref = new_node
+        self.start_node = new_node
+
+    def insert_at_end(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            return
+        n = self.start_node
+        while n.nref is not None:
+            n = n.nref
+        new_node = Node(data)
+        n.nref = new_node
+        new_node.pref = n
+
+    def delete_at_start(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        if self.start_node.nref is None:
+            self.start_node = None
+            return
+        self.start_node = self.start_node.nref
--- Game/Snake.py
+++ Game/Snake.py
@@ -1,59 +1,164 @@
 import curses
 import random
-import curses
+import time
+from curses import textpad
 
-s = curses.initscr()
-curses.curs_set(0)
-sh, sw = s.getmaxyx()
-w = curses.newwin(sh, sw, 0, 0)
-w.keypad(1)
-w.timeout(100)
+from Game.Doublylinkedlist import DoublyLinkedList, Node
 
-snk_x = sw/4
-snk_y = sh/2
-snake = [
-    [snk_y, snk_x],
-    [snk_y, snk_x-1],
-    [snk_y, snk_x-2]
-]
 
-food = [sh/2, sw/2]
-w.addch(int(food[0]), int(food[1]), curses.ACS_PI)
+def create_food(snake, box):
+    """Simple function to find coordinates of food which is inside box and not on snake body"""
+    food = None
+    while food is None:
+        food = [random.randint(box[0][0] + 1, box[1][0] - 1),
+                random.randint(box[0][1] + 1, box[1][1] - 1)]
+        if food in snake:
+            food = None
+    return food
 
-key = curses.KEY_RIGHT
 
-while True:
-    next_key = w.getch()
-    key = key if next_key == -1 else next_key
+def create_poison(snake, box):
+    """Simple function to find coordinates of food which is inside box and not on snake body"""
+    poison = None
+    while poison is None:
+        poison = [random.randint(box[0][0] + 1, box[1][0] - 1),
+                  random.randint(box[0][1] + 1, box[1][1] - 1)]
+        if poison in snake:
+            poison = None
+    return poison
 
-    if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw] or snake[0] in snake[1:]:
-        curses.endwin()
-        quit()
 
-    new_head = [snake[0][0], snake[0][1]]
+def gamer(stdscr):
+    # initial settings
+    global new_head
+    stdscr.clear()
+    curses.curs_set(0)
+    stdscr.nodelay(1)
+    stdscr.timeout(100)
 
-    if key == curses.KEY_DOWN:
-        new_head[0] += 1
-    if key == curses.KEY_UP:
-        new_head[0] -= 1
-    if key == curses.KEY_LEFT:
-        new_head[1] -= 1
-    if key == curses.KEY_RIGHT:
-        new_head[1] += 1
+    # create a game box
+    sh = 30
+    sw = 90
+    box = [[3, 3], [sh - 3, sw - 3]]  # [[ul_y, ul_x], [dr_y, dr_x]]
+    textpad.rectangle(stdscr, box[0][0], box[0][1], box[1][0], box[1][1])
 
-    snake.insert(0, new_head)
+    linked_list = DoublyLinkedList()
+    linked_list.insert_in_emptylist([sh // 2, sw // 2 + 1])
+    linked_list.insert_at_start([sh // 2, sw // 2])
+    linked_list.insert_at_start([sh // 2, sw // 2 - 1])
 
-    if snake[0] == food:
-        food = None
-        while food is None:
-            nf = [
-                random.randint(1, sh-1),
-                random.randint(1, sw-1)
-            ]
-            food = nf if nf not in snake else None
-        w.addch(food[0], food[1], curses.ACS_PI)
-    else:
-        tail = snake.pop()
-        w.addch(int(tail[0]), int(tail[1]), ' ')
+    # create snake and set initial direction
+    snake = [[sh // 2, sw // 2 + 1], [sh // 2, sw // 2], [sh // 2, sw // 2 - 1]]
+    direction = curses.KEY_RIGHT
 
-    w.addch(int(snake[0][0]), int(snake[0][1]), curses.ACS_CKBOARD)
+    # draw snake
+    for y, x in snake:
+        stdscr.addstr(y, x, '#')
+
+    n = linked_list.start_node
+    f = 10
+    g = 10
+    while n is not None:
+        stdscr.addstr(n.item[0], n.item[1], str(n.item))
+        f = f + 3
+        g = g + 3
+        n = n.nref
+
+    # create food
+    food = create_food(snake, box)
+    stdscr.addstr(food[0], food[1], '+')
+    poison = create_poison(snake, box)
+    stdscr.addstr(poison[0], poison[1], '*')
+
+    # print score
+    score = 0
+    score_text = "Score: {}".format(score)
+    stdscr.addstr(1, sw // 2 - len(score_text) // 2, score_text)
+
+    while 1:
+        # non-blocking input
+        key = stdscr.getch()
+
+        # set direction if user pressed any arrow key
+        if key in [curses.KEY_RIGHT, curses.KEY_LEFT, curses.KEY_DOWN, curses.KEY_UP]:
+            direction = key
+
+        # find next position of snake head
+        head = snake[0]
+        if direction == curses.KEY_RIGHT:
+            new_head = [head[0], head[1] + 1]
+        elif direction == curses.KEY_LEFT:
+            new_head = [head[0], head[1] - 1]
+        elif direction == curses.KEY_DOWN:
+            new_head = [head[0] + 1, head[1]]
+            time.sleep(0.1)
+        elif direction == curses.KEY_UP:
+            new_head = [head[0] - 1, head[1]]
+            time.sleep(0.1)
+
+        # insert and print new head time.sleep(0.5)
+        stdscr.addstr(new_head[0], new_head[1], '#')
+        snake.insert(0, new_head)
+
+        # remove head in this coordinates
+
+        # if sanke head is on food
+        if snake[0] == food:
+
+            # update score
+            score += 1
+            score_text = "Score: {}".format(score)
+            stdscr.addstr(1, sw // 2 - len(score_text) // 2, score_text)
+
+            # create new food
+            food = create_food(snake, box)
+            stdscr.addstr(food[0], food[1], '+')
+
+            # increase speed of game
+            stdscr.timeout(100 - (len(snake) // 3) % 90)
+        elif snake[0] == poison:
+            # update score
+            score -= 1
+            score_text = "Score: {}".format(score)
+            stdscr.addstr(1, sw // 2 - len(score_text) // 2, score_text)
+
+            temp = snake[len(snake) - 1]
+
+            stdscr.addstr(temp[0], temp[1], '/')
+            snake.pop(len(snake) - 1)
+
+            # create new poison
+            poison = create_poison(snake, box)
+            stdscr.addstr(poison[0], poison[1], '*')
+
+            # increase speed of game
+            stdscr.timeout(100 - (len(snake) // 3) % 90)
+            stdscr.addstr(snake[-1][0], snake[-1][1], ' ')
+            snake.pop()
+            stdscr.refresh()
+
+        else:
+            # shift snake's tail
+            stdscr.addstr(snake[-1][0], snake[-1][1], ' ')
+            snake.pop()
+
+        # conditions for game over
+        if (snake[0][0] in [box[0][0], box[1][0]] or
+                snake[0][1] in [box[0][1], box[1][1]] or
+                snake[0] in snake[1:]):
+            msg = "Game Over!"
+
+            stdscr.addstr(sh // 2, sw // 2 - len(msg) // 2, msg)
+
+            while 1:
+                key = stdscr.getch()
+                if key == curses.KEY_ENTER or key in [10, 13]:
+                    break
+
+            stdscr.nodelay(0)
+            stdscr.getch()
+            break
+
+
+class Snake:
+    pass
